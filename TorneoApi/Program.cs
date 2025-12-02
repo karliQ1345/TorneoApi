ea76b15d4319fee99833c302f2d6500154174979
Cristhiansito0 <cristhianguajan2@gmail.com>
2025-12-02 13:40:38 -0500

    Creacion del dockerfile para subir a render

--- TorneoApi/Program.cs
+++ TorneoApi/Program.cs
@@ -22,7 +22,7 @@ namespace TorneoApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            //if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
