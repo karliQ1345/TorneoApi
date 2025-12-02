c9355d77446472d80b42e6e18049b0479ae0cb5b
Cristhiansito0 <cristhianguajan2@gmail.com>
2025-12-02 12:10:48 -0500

    Creacion del modelado del proyecto

---
+++ TorneoApi/Models/Equipo.cs
@@ -0,0 +1,13 @@
+﻿namespace TorneoApi.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Entrenador { get; set; } = string.Empty;
+
+        // Relaciones
+        public List<Jugador> Jugadores { get; set; } = new();
+        public List<Inscripcion> Inscripciones { get; set; } = new();
+    }
+}
---
+++ TorneoApi/Models/Partido.cs
@@ -0,0 +1,28 @@
+﻿using TorneoApi.Models.Enums;
+namespace TorneoApi.Models
+{
+    public class Partido
+    {
+        public int Id { get; set; }
+
+        public int TorneoId { get; set; }
+        public Torneo? Torneo { get; set; }
+
+        // Equipos enfrentados
+        public int EquipoLocalId { get; set; }
+        public Equipo? EquipoLocal { get; set; }
+
+        public int EquipoVisitanteId { get; set; }
+        public Equipo? EquipoVisitante { get; set; }
+
+        public DateTime FechaProgramada { get; set; }
+        public FasePartido Fase { get; set; } // Grupos, Final, etc.
+        public string? Grupo { get; set; } // "A" (si es fase de grupos)
+
+        // Resultados
+        public int GolesLocal { get; set; } = 0;
+        public int GolesVisitante { get; set; } = 0;
+        public bool Jugado { get; set; } = false; // ¿Ya se jugó?
+
+    }
+}
